320ace07129dfd1eba68e652e8a6a3d8505081a1
mostafaKhaled1991 <moskhaa@gmail.com>
2023-05-21 17:58:46 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      body: Center(
+        child: Text("Welcome Flutter"),
+      ),
+    ),
+  ));
+}
